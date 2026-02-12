e2f3ac4283eda066ee7472ea71257233fbe112d7
duc15112003 <ducpm15112003@gmail.com>
2026-02-12 11:12:04 +0700

    change name site

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001';
+const URL = import.meta.env.PROD ? window.location.origin : (import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001');
 
 class SocketService {
     socket = null;
